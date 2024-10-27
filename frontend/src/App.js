30b1a7065372baae47bd334438bbbff6c6446e0c
FacundoOrihuela <117322261+FacundoOrihuela@users.noreply.github.com>
2024-10-27 17:52:20 -0300

    Cambios hechos :)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,9 @@
 import "./App.css";
-import React, { useState } from "react";
 
 function App() {
-  const [nombre, setNombre] = useState("");
-
-  const guardarNombre = (e) => {
-    setNombre(e.target.value);
-  };
-
   return (
     <div className="App">
       <h1> Hello World! </h1>
-      <input value={nombre} placeholder="Ingrese Nombre" onChange={guardarNombre}></input>
-    <p> {nombre}</p>
     </div>
   );
 }
