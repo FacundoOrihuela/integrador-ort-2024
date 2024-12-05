86a71848e7abbd07d650fda101a342936c1cdd02
FacundoOrihuela <117322261+FacundoOrihuela@users.noreply.github.com>
2024-12-04 21:46:15 -0300

    x

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,9 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { saveSessionToken } from '../features/loginSlice';
-import { UserContext } from '../context/UserContext';
+import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { saveSessionToken } from "../features/loginSlice";
+import { UserContext } from "../context/UserContext";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -12,22 +12,38 @@ const Header = () => {
 
   const handleLogout = () => {
     dispatch(saveSessionToken(null));
-    localStorage.removeItem("token"); 
+    localStorage.removeItem("token");
     localStorage.removeItem("idUsuarioLogueado");
     navigate("/");
   };
 
+
   return (
     <div className="header">
-      <div className="header-link" onClick={handleLogout}>Cerrar sesión</div>
+      <div className="header-link" onClick={handleLogout}>
+        Cerrar sesión
+      </div>
       <div className="header-left">
-        <Link to="/principal" className="header-link">Home</Link>
-        <Link to="/somos" className="header-link">Somos</Link>
-        <Link to="/events" className="header-link">Eventos</Link>
-        <Link to="/productos" className="header-link">Productos</Link>
-        <Link to="/store" className="header-link">Tienda Online</Link>
+        <Link to="/principal" className="header-link">
+          Home
+        </Link>
+        <Link to="/somos" className="header-link">
+          Somos
+        </Link>
+        <Link to="/events" className="header-link">
+          Eventos
+        </Link>
+        <Link to="/productos" className="header-link">
+          Productos
+        </Link>
+        <Link to="/store" className="header-link">
+          Tienda Online
+        </Link>
+
         {user && user.userType === "administrator" && (
-          <Link to="/admin-panel" className="header-link">Panel Administrativo</Link>
+          <Link to="/admin-panel" className="header-link">
+            Panel Administrativo
+          </Link>
         )}
       </div>
       <div className="header-right">
