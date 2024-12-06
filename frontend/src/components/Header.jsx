07e2253f5922d993253677e8ad86212f3399c2fe
Juan Nuñez <tayet.nunez@gmail.com>
2024-12-06 18:20:37 -0300

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,30 +1,60 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FaUserCircle } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+import { FaUserCircle } from "react-icons/fa";
 import logoImg from "../components/img/logo.png";
+import React, { useContext, useState } from "react";
+import Cart from "./ecommerce/Cart";
+import { UserContext } from "../context/UserContext";
 
 const Header = () => {
-    return (
-        <header className="bg-orange-600 shadow-md">
-            <div className="container mx-auto flex justify-between items-center py-6 px-8">
-                <div className="flex items-center">
-                    <img src={logoImg} alt="Logo" className="h-14 w-14 rounded-full" /> {/* Logo redondo */}
-                    <span className="ml-3 text-2xl font-bold text-black">Tiféret</span>
-                </div>
-                <nav className="space-x-6">
-                    <Link to="/" className="text-black hover:text-gray-700 text-lg">Home</Link>
-                    <Link to="/somos" className="text-black hover:text-gray-700 text-lg">Somos</Link>
-                    <Link to="/events" className="text-black hover:text-gray-700 text-lg">Actividades</Link> 
-                    <Link to="/grupos" className="text-black hover:text-gray-700 text-lg">Grupos</Link>
-                    <Link to="/tienda" className="text-black hover:text-gray-700 text-lg">Tienda</Link>
-                    <Link to="/aula" className="text-black hover:text-gray-700 text-lg">Aula virtual</Link>
-                </nav>
-                <div>
-                    <FaUserCircle className="text-black text-3xl"/>
-                </div>
-            </div>
-        </header>
-    );
+  const { user } = useContext(UserContext);
+  const [showCart, setShowCart] = useState(false);
+
+  return (
+    <header className="bg-orange-600 shadow-md">
+      <div className="container mx-auto flex justify-between items-center py-6 px-8">
+        <div className="flex items-center">
+          <img src={logoImg} alt="Logo" className="h-14 w-14 rounded-full" />{" "}
+          {/* Logo redondo */}
+          <span className="ml-3 text-2xl font-bold text-black">Tiféret</span>
+        </div>
+        <nav className="space-x-6">
+          <Link to="/" className="text-black hover:text-gray-700 text-lg">
+            Home
+          </Link>
+          <Link to="/somos" className="text-black hover:text-gray-700 text-lg">
+            Somos
+          </Link>
+          <Link to="/events" className="text-black hover:text-gray-700 text-lg">
+            Actividades
+          </Link>
+          <Link to="/grupos" className="text-black hover:text-gray-700 text-lg">
+            Grupos
+          </Link>
+          <Link to="/tienda" className="text-black hover:text-gray-700 text-lg">
+            Tienda
+          </Link>
+          <Link to="/aula" className="text-black hover:text-gray-700 text-lg">
+            Aula virtual
+          </Link>
+          {user && user.userType === "administrator" && (
+            <Link to="/admin-panel" className="header-link">
+              Panel Administrativo
+            </Link>
+          )}
+        </nav>
+        <div>
+          <button
+            className="header-button"
+            onClick={() => setShowCart(!showCart)}
+          >
+            🛒
+          </button>
+          <FaUserCircle className="text-black text-3xl" />
+        </div>
+        {showCart && <Cart />}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
