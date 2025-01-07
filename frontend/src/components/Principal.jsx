0a057788c202066befd810ed67cd20ace6490c9f
Juan Nuñez <tayet.nunez@gmail.com>
2025-01-07 18:06:52 -0300

    principal redesign

--- frontend/src/components/Principal.jsx
+++ frontend/src/components/Principal.jsx
@@ -1,102 +1,303 @@
 import { Link } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
 import Header from "./Header";
-import Footer from "./Footer"; // Importamos el Footer
+import Footer from "./Footer";
 import portadaImg from "../components/img/Portada.png";
 import kabalaImg from "../components/img/kabala.png";
 import meditarImg from "../components/img/meditar.png";
 import circulosImg from "../components/img/circulos.png";
 import integracionesImg from "../components/img/integraciones.png";
 import psicoterapiaImg from "../components/img/psicoterapias.png";
-import fotoHomeImg from "../components/img/fotoHome.png";
 
 const Principal = () => {
-
   return (
-    <div className="min-h-screen flex flex-col">
+    <Box sx={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
       <Header />
-      <div className="flex-grow mt-[5rem]"> {/* Asegura que el contenido crezca */}
-        <section className="bg-gray py-8">
-          <div className="container w-[50%] mx-auto flex justify-center space-x-8">
-            <Link to="/kabala" className="text-center flex flex-col items-center">
-              <div className="bg-white rounded-full w-[100px] h-[100px] flex justify-center items-center">
-                <img src={kabalaImg} alt="Escuela Kabalá Viva" className="mx-auto cursor-pointer rounded-full w-[70%] h-[70%]" />
-              </div>
-              <p className="mt-2">Escuela Kabalá Viva</p>
-            </Link>
+      <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "column" }}>
+        <Box
+          sx={{
+            position: "fixed",
+            left: 0,
+            top: "50%",
+            transform: "translateY(-50%)",
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            zIndex: 50,
+          }}
+        >
+          <Link
+            to="/kabala"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              textDecoration: "none",
+            }}
+          >
+            <Box
+              sx={{
+                width: 100,
+                height: 100,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                position: "relative",
+                "&:hover .button-text": {
+                  opacity: 1,
+                },
+              }}
+            >
+              <img
+                src={kabalaImg}
+                alt="Escuela Kabalá Viva"
+                style={{ width: "70%", height: "70%", borderRadius: "50%" }}
+              />
+              <Typography
+                sx={{
+                  ml: 2,
+                  color: "white",
+                  position: "absolute",
+                  left: "110%",
+                  opacity: 0,
+                  transition: "opacity 0.3s",
+                }}
+                className="button-text"
+              >
+                Escuela Kabalá Viva
+              </Typography>
+            </Box>
+          </Link>
 
-            <Link to="/circulos" className="text-center flex flex-col items-center">
-              <div className="bg-white rounded-full w-[100px] h-[100px] flex justify-center items-center">
-                <img src={meditarImg} alt="Círculos de crecimiento" className="mx-auto cursor-pointer rounded-full w-[70%] h-[70%]" />
-              </div>
-              <p className="mt-2">Círculos de crecimiento</p>
-            </Link>
+          <Link
+            to="/yoga"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              textDecoration: "none",
+            }}
+          >
+            <Box
+              sx={{
+                width: 100,
+                height: 100,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                position: "relative",
+                "&:hover .button-text": {
+                  opacity: 1,
+                },
+              }}
+            >
+              <img
+                src={circulosImg}
+                alt="Bienestar corporal"
+                style={{ width: "70%", height: "70%", borderRadius: "50%" }}
+              />
+              <Typography
+                sx={{
+                  ml: 2,
+                  color: "white",
+                  position: "absolute",
+                  left: "110%",
+                  opacity: 0,
+                  transition: "opacity 0.3s",
+                }}
+                className="button-text"
+              >
+                Círculos de crecimiento
+              </Typography>
+            </Box>
+          </Link>
 
-            <Link to="/yoga" className="text-center flex flex-col items-center">
-              <div className="bg-white rounded-full w-[100px] h-[100px] flex justify-center items-center">
-                <img src={circulosImg} alt="Bienestar corporal" className="mx-auto cursor-pointer rounded-full w-[70%] h-[70%]" />
-              </div>
-              <p className="mt-2">Bienestar corporal </p>
-            </Link>
+          <Link
+            to="/circulos"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              textDecoration: "none",
+            }}
+          >
+            <Box
+              sx={{
+                width: 100,
+                height: 100,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                position: "relative",
+                "&:hover .button-text": {
+                  opacity: 1,
+                },
+              }}
+            >
+              <img
+                src={meditarImg}
+                alt="Círculos de crecimiento"
+                style={{ width: "70%", height: "70%", borderRadius: "50%" }}
+              />
+              <Typography
+                sx={{
+                  ml: 2,
+                  color: "white",
+                  position: "absolute",
+                  left: "110%",
+                  opacity: 0,
+                  transition: "opacity 0.3s",
+                }}
+                className="button-text"
+              >
+                Bienestar corporal
+              </Typography>
+            </Box>
+          </Link>
 
-            <Link to="/integraciones" className="text-center flex flex-col items-center">
-              <div className="bg-white rounded-full w-[100px] h-[100px] flex justify-center items-center">
-                <img src={integracionesImg} alt="Abordajes holísticos" className="mx-auto cursor-pointer rounded-full w-[70%] h-[70%]" />
-              </div>
-              <p className="mt-2">Abordajes holísticos</p>
-            </Link>
+          <Link
+            to="/integraciones"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              textDecoration: "none",
+            }}
+          >
+            <Box
+              sx={{
+                width: 100,
+                height: 100,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                position: "relative",
+                "&:hover .button-text": {
+                  opacity: 1,
+                },
+              }}
+            >
+              <img
+                src={integracionesImg}
+                alt="Abordajes holísticos"
+                style={{ width: "70%", height: "70%", borderRadius: "50%" }}
+              />
+              <Typography
+                sx={{
+                  ml: 2,
+                  color: "white",
+                  position: "absolute",
+                  left: "110%",
+                  opacity: 0,
+                  transition: "opacity 0.3s",
+                }}
+                className="button-text"
+              >
+                Abordajes holísticos
+              </Typography>
+            </Box>
+          </Link>
 
-            <Link to="/psicoterapias" className="text-center flex flex-col items-center">
-              <div className="bg-white rounded-full w-[100px] h-[100px] flex justify-center items-center">
-                <img src={psicoterapiaImg} alt="Psicoterapia" className="mx-auto cursor-pointer rounded-full w-[70%] h-[70%]" />
-              </div>
-              <p className="mt-2">Psicoterapia</p>
-            </Link>
-            <Link to="/consultorios" className="text-center flex flex-col items-center">
-              <div className="bg-white rounded-full w-[100px] h-[100px] flex justify-center items-center">
-                <img src={psicoterapiaImg} alt="Consultorios" className="mx-auto cursor-pointer rounded-full w-[70%] h-[70%]" />
-              </div>
-              <p className="mt-2">Consultorios</p>
-            </Link>
-          </div>
-        </section>
-        <section className="relative">
-          <img
+          <Link
+            to="/psicoterapias"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              textDecoration: "none",
+            }}
+          >
+            <Box
+              sx={{
+                width: 100,
+                height: 100,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                position: "relative",
+                "&:hover .button-text": {
+                  opacity: 1,
+                },
+              }}
+            >
+              <img
+                src={psicoterapiaImg}
+                alt="Psicoterapia"
+                style={{ width: "70%", height: "70%", borderRadius: "50%" }}
+              />
+              <Typography
+                sx={{
+                  ml: 2,
+                  color: "white",
+                  position: "absolute",
+                  left: "110%",
+                  opacity: 0,
+                  transition: "opacity 0.3s",
+                }}
+                className="button-text"
+              >
+                Psicoterapia
+              </Typography>
+            </Box>
+          </Link>
+
+          <Link
+            to="/consultorios"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              textDecoration: "none",
+            }}
+          >
+            <Box
+              sx={{
+                width: 100,
+                height: 100,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                position: "relative",
+                "&:hover .button-text": {
+                  opacity: 1,
+                },
+              }}
+            >
+              <img
+                src={psicoterapiaImg}
+                alt="Consultorios"
+                style={{ width: "70%", height: "70%", borderRadius: "50%" }}
+              />
+              <Typography
+                sx={{
+                  ml: 2,
+                  color: "white",
+                  position: "absolute",
+                  left: "110%",
+                  opacity: 0,
+                  transition: "opacity 0.3s",
+                }}
+                className="button-text"
+              >
+                Consultorios
+              </Typography>
+            </Box>
+          </Link>
+        </Box>
+
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Box
+            component="img"
             src={portadaImg}
             alt="Yoga Session"
-            className="w-full"
+            sx={{ width: "100%", height: "100%", objectFit: "contain" }}
           />
-        </section>
-        <section className="container mx-auto py-8 px-6">
-          <h2 className="text-2xl font-bold mb-4">Nuestra Historia</h2>
-          <div className="flex flex-col md:flex-row">
-            <div className="md:w-1/2">
-              <img
-                src={fotoHomeImg}
-                alt="Nuestra Historia"
-                className="w-full"
-              />
-            </div>
-            <div className="md:w-1/2 md:pl-8 mt-4 md:mt-0">
-              <p>
-                El Centro Tiféret surgió de un anhelo profundo del alma. Muchas
-                años antes de su materialización, ya existía en mi interior con
-                creciente intensidad la voluntad de crear un lugar que pudiera dar
-                cabida a diferentes formas de abordar la sanación, guiadas siempre
-                por el espíritu de equilibrio, armonía y belleza que inspira su
-                nombre.
-              </p>
-              <p>
-                En el plano cotidiano, me desempeño como terapeuta individual, de
-                parejas y de grupos. También facilito una Maestría en Grupos de
-                Crecimiento Inspirados en la sabiduría de la Kabalá, así como los
-                Círculos Sagrados.
-              </p>
-            </div>
-          </div>
-        </section>
-      </div>
+        </Box>
+      </Box>
       <Footer />
-    </div>
+    </Box>
   );
 };
 
